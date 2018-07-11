ed205217e6a97397f14b1ae15d4ac118b378c6f2
spinningplayer <Thijsvanmarle@gmail.com>
2018-07-11 10:42:42 +0200

    Added event trigger
    Added an event trigger on GPIO 1 to play the video when it rises

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 from omxplayer.player import OMXPlayer
 from pathlib import Path
+from wand.image import Image
+import RPi.GPIO as GPIO
+import wand
 
 app = Flask(__name__)
 player = None
-path = Path("/home/pi/Videos/Girl.mp4")
+path = Path("/home/pi/Videos/Video.mp4")
+imagePath = Path("./nee.png")
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(1, GPIO.IN, pull_up_down = GPIO.PUD_OFF)
 
 
 @app.route('/')
@@ -37,6 +45,27 @@ def stop():
         print(e)
         return "error"
 
+@app.route('/display')
+def display():
+    # try:
+    #     image = request.args.get('path')
+    #     imagePath = Path(image)
+    # except:
+    #     return "Invalid input"
+
+    try:
+        with Image(filename='nee.png') as img:
+            wand.display.display(img)
+        return "success"
+    except Exception as e:
+        print(e)
+        return "error: couldn't display image"
+
+
+GPIO.add_event_detect(1, GPIO.RISING, callback=play)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=1337)
+
+GPIO.cleanup()
